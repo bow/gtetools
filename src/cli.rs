f1c2d3ab886c0f90c99c7325f0812e760258ffc6
bow <bow@bow.web.id>
2017-03-21 23:23:36 +0100

    Update formatting with rustfmt

--- src/cli.rs
+++ src/cli.rs
@@ -38,43 +38,40 @@ pub fn build_cli() -> App<'static, 'static> {
         .about(ABOUT)
         .template(TEMPLATE)
         .max_term_width(80)
-        .settings(&[
-            AppSettings::GlobalVersion,
-            AppSettings::SubcommandRequiredElseHelp,
-            AppSettings::DisableHelpSubcommand,
-            AppSettings::VersionlessSubcommands])
-        .subcommand(
-            SubCommand::with_name("stats")
-                .about("Gathers various statistics")
-                .template(TEMPLATE_SUBCMD)
-                .arg(Arg::with_name("input")
-                        .value_name("input")
-                        .help("Path to input annotation file or '-' for stdin")
-                        .takes_value(true)
-                        .required(true)))
-        .subcommand(
-            SubCommand::with_name("gtf2refflat")
-                .about("Converts from the GTF format to refFlat")
-                .template(TEMPLATE_SUBCMD)
-                .arg(Arg::with_name("input")
-                        .value_name("input")
-                        .help("Path to input annotation file or '-' for stdin")
-                        .takes_value(true)
-                        .required(true))
-                .arg(Arg::with_name("output")
-                        .value_name("output")
-                        .help("Path to output annotation file or '-' for stdout")
-                        .takes_value(true)
-                        .required(true)))
+        .settings(&[AppSettings::GlobalVersion,
+                    AppSettings::SubcommandRequiredElseHelp,
+                    AppSettings::DisableHelpSubcommand,
+                    AppSettings::VersionlessSubcommands])
+        .subcommand(SubCommand::with_name("stats")
+                        .about("Gathers various statistics")
+                        .template(TEMPLATE_SUBCMD)
+                        .arg(Arg::with_name("input")
+                                 .value_name("input")
+                                 .help("Path to input annotation file or '-' for stdin")
+                                 .takes_value(true)
+                                 .required(true)))
+        .subcommand(SubCommand::with_name("gtf2refflat")
+                        .about("Converts from the GTF format to refFlat")
+                        .template(TEMPLATE_SUBCMD)
+                        .arg(Arg::with_name("input")
+                                 .value_name("input")
+                                 .help("Path to input annotation file or '-' for stdin")
+                                 .takes_value(true)
+                                 .required(true))
+                        .arg(Arg::with_name("output")
+                                 .value_name("output")
+                                 .help("Path to output annotation file or '-' for stdout")
+                                 .takes_value(true)
+                                 .required(true)))
 }
 
 /// Runs the appropriate tool given the subcommand argument matches.
 pub fn run(matches: ArgMatches) -> Result<(), &'static str> {
     match matches.subcommand() {
-        ("stats", Some(m)) =>       tools::stats::run(m),
+        ("stats", Some(m)) => tools::stats::run(m),
         ("gtf2refflat", Some(m)) => tools::convert_gtf2refflat::run(m),
         // We should not reach this point since we already require
         // that subcommands must be present in the app settings.
-        _   => Err("Unexpected subcommand parsing error")
+        _ => Err("Unexpected subcommand parsing error"),
     }
 }
